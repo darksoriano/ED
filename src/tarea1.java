298cd1c6d0fdcd808a90fcc78bba69b5e834a1f8
darksoriano <aspirina1996>
2017-03-21 08:49:49 +0100

    rename

--- src/tarea1.java
+++ src/tarea1.java
@@ -10,4 +10,31 @@
  */
 public class tarea1 {
     
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Staff staff1 = new Staff("-Peter");
+        staff1.setHoursWorked(160);
+        int pay = staff1.calculatePay(1000,400);
+        System.out.println("pay= " + pay);
+
+        Staff staff2 = new Staff("-Jane","Lee");
+        staff2.setHoursWorked(160);
+        pay=staff2.calculatePay();
+        System.out.println("Pay = "+ pay);
+        
+        System.out.println("\n\nUpdating Jane's hours worked to -10");
+        staff2.setHoursWorked(-10);
+        System.out.println("\nHours Worked = "+ staff2.getHoursWorked());
+        pay = staff2.calculatePay();
+        System.out.println("Pay =" +pay);
+        
+        
+        
+
+        // TODO code application logic here
+    }
+    
 }
